5723e13655be45d4ce4997092524471a5c5f7db0
Stuart Attenborrow <sattenborrow@datalive.com.au>
2017-01-25 12:15:24 +1100

    Added placeholder mac hardware source

---
+++ hardware/hardware_darwin.go
@@ -0,0 +1,30 @@
+package hardware
+
+import (
+	"path"
+)
+
+func PlatformName() string {
+	return "mac"
+}
+
+func RendererPath() string {
+	return path.Join("Blender", "blender.app", "Contents", "MacOS", "blender")
+}
+
+func CpuStat() *Cpu {
+	cpu := Cpu{}
+
+	// TODO: implement mac stats
+
+	return &cpu
+}
+
+func GpuStat() *Gpu {
+	return &Gpu {}
+}
+
+func TotalMemory() uint64 {
+	// TODO: Calculate total memory
+	return 8350000
+}
